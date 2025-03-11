295ac870beb75f0ab03211ee7f7a696d38885238
Luis Riquelme <juan.riquelme@brain.mpg.de>
2025-03-11 12:20:12 +0100

    more consistent naming of sel_ and is_ functions

--- nocte/df_wrapper.py
+++ nocte/df_wrapper.py
@@ -99,6 +99,23 @@ class DataFrameWrapper:
             self.reg.loc[mask]
         )
 
+    @staticmethod
+    def _masks(criterias, *, how='all', invert=False):
+        """
+        Combine multiple boolean masks
+        """
+        assert how in ('all', 'any')
+
+        if how == 'all':
+            mask = np.all(criterias, axis=0)
+        else:
+            mask = np.any(criterias, axis=0)
+
+        if invert:
+            mask = ~mask
+
+        return mask
+
     def sel_mask(self, mask, /, invert=False):
         """
         Select using a boolean mask
@@ -108,18 +125,21 @@ class DataFrameWrapper:
 
         return self._apply_mask(mask)
 
-    def sel_masks(self, criterias, *, how='all', invert=False):
+    def is_match(self, *, how='all', invert=False, **col_values):
         """
-        Select using multiple boolean masks to be combined
+        Return a mask of direct comparison of some column.
+
+        We allow to select for missing values by using np.nan.
+
+        For example:
+            wins.match(cat='baseline')
         """
-        assert how in ('all', 'any')
+        criterias = [
+            (self.reg[col] == value) if not pd.isna(value) else self.reg[col].isna()
+            for col, value in col_values.items()
+        ]
 
-        if how == 'all':
-            mask = np.all(criterias, axis=0)
-        else:
-            mask = np.any(criterias, axis=0)
-
-        return self.sel_mask(mask, invert=invert)
+        return self._masks(criterias, how=how, invert=invert)
 
     def sel_match(self, *, how='all', invert=False, **col_values):
         """
@@ -130,12 +150,21 @@ class DataFrameWrapper:
         For example:
             wins.sel(cat='baseline')
         """
+        mask = self.is_match(how=how, invert=invert, **col_values)
+        return self.sel_mask(mask)
+
+    def is_between(self, *, how='all', invert=False, **col_ranges):
+        """
+        Return a mask that checks that some columns are within the given range.
+        For example:
+            wins.between(duration=(0, 60_000))
+        """
         criterias = [
-            (self.reg[col] == value) if not pd.isna(value) else self.reg[col].isna()
-            for col, value in col_values.items()
+            self.reg[col].between(*vrange)
+            for col, vrange in col_ranges.items()
         ]
 
-        return self.sel_masks(criterias, how=how, invert=invert)
+        return self._masks(criterias, how=how, invert=invert)
 
     def sel_between(self, *, invert=False, how='all', **col_ranges):
         """
@@ -143,25 +172,30 @@ class DataFrameWrapper:
         For example:
             wins.sel_between(duration=(0, 60_000))
         """
-        criterias = [
-            self.reg[col].between(*value_range)
-            for col, value_range in col_ranges.items()
-        ]
+        mask = self.is_between(how=how, invert=invert, **col_ranges)
+        return self.sel_mask(mask)
 
-        return self.sel_masks(criterias, how=how, invert=invert)
-
-    def sel_isin(self, *, invert=False, how='all', **col_values):
+    def is_in(self, *, how='all', invert=False, **col_values):
         """
-        Select by direct comparison of some column where any of the values are acceptable.
+        Return a mask of direct comparison of some column where any of the values are acceptable.
         For example:
-            wins.sel_isin(cat=['sws', 'rem'])
+            wins.isin(cat=['sws', 'rem'])
         """
         criterias = [
             self.reg[col].isin(values)
             for col, values in col_values.items()
         ]
 
-        return self.sel_masks(criterias, how=how, invert=invert)
+        return self._masks(criterias, how=how, invert=invert)
+
+    def sel_in(self, *, invert=False, how='all', **col_values):
+        """
+        Select by direct comparison of some column where any of the values are acceptable.
+        For example:
+            wins.sel_isin(cat=['sws', 'rem'])
+        """
+        mask = self.is_in(invert=invert, how=how, **col_values)
+        return self.sel_mask(mask)
 
     @functools.wraps(pd.DataFrame.sample)
     def sample(self, *args, **kwargs):
